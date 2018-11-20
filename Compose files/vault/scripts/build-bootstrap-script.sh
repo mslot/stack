e67c5d62abce930c93cc47eb4a3a6c0497294e52
Martin Slot <slot@sigsys.dk>
2018-11-20 20:23:23 +0100

    Added correct paths and correct rm for final script

--- Compose files/vault/scripts/build-bootstrap-script.sh
+++ Compose files/vault/scripts/build-bootstrap-script.sh
@@ -3,5 +3,5 @@
 token=$(docker logs `docker ps -q --filter "name=vault-server-0"` 2>&1 | awk 'BEGIN {FS=":"} /Root Token/ {print $2}')
 token=$(echo "$token" | xargs)
 echo "found token: $token"
-awk -v token="$token" '{ gsub("{{root_token}}",token); print $0}' bootstrap-dev.template.sh > bootstrap-dev.final.sh
+awk -v token="$token" '{ gsub("{{root_token}}",token); print $0}' vault/scripts/bootstrap-dev.template.sh > vault/scripts/bootstrap-dev.final.sh
 echo "built bootstrap script"
